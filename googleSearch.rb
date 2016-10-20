3d668b29a6bb460f530a1209063bc77e66e98ee8
lamhotjm <lamhot.siagian@modalku.co.id>
2016-10-20 13:43:44 +0700

    add simple test

---
+++ googleSearch.rb
@@ -0,0 +1,20 @@
+require 'rubygems'
+require 'selenium-webdriver'
+
+chromedriver_path = File.join(File.absolute_path('/', File.dirname(__FILE__)),"","chromedriver.exe")
+Selenium::WebDriver::Chrome.driver_path = chromedriver_path
+
+driver = Selenium::WebDriver.for :chrome
+driver.get "http://google.com"
+
+element = driver.find_element :name => "q"
+element.send_keys "Lamhot JM Siagian"
+element.submit
+
+puts "Page title is #{driver.title}"
+
+wait = Selenium::WebDriver::Wait.new(:timeout => 10)
+wait.until { driver.title.downcase.start_with? "cheese!" }
+
+puts "Page title is #{driver.title}"
+driver.quit
